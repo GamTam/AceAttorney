94a5c6c10baa460180e9337441f646a14cc1b9f7
Mabel DaCambra <luccadacambra@gmail.com>
2022-11-19 17:33:33 -0500

    Updated songs

--- Assets/Scripts/Izzy/Scriptable Objects/TrialSO.cs
+++ Assets/Scripts/Izzy/Scriptable Objects/TrialSO.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Trial", menuName = "AceAttorney GDW/Trial")]
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "AceAttorney GDW/Trial")]
 public class TrialSO : ScriptableObject 
 {
     public string trialName;
