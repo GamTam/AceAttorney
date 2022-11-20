31d4ec2f794d20fbb9d5b8f9876952f024e314e2
Mabel DaCambra <luccadacambra@gmail.com>
2022-11-19 23:08:02 -0500

    Updated Music Timing

--- Assets/Scripts/Izzy/Scriptable Objects/DialogueSO.cs
+++ Assets/Scripts/Izzy/Scriptable Objects/DialogueSO.cs
@@ -5,5 +5,8 @@ public class DialogueSO : ScriptableObject
 {
     public bool isPresentable;
     public EvidenceSO evidence = null;
+    public string name;
     [TextArea(10, 5)] public string dialogueText;
+    public DialogueSO prevLine;
+    public DialogueSO nextLine;
 }
