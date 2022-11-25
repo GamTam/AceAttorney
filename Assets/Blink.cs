7544e3efd85dbe3f8ca437c11f82e0378ebcfd55
GamTam <luccadacambra@gmail.com>
2022-11-24 22:43:34 -0500

    Random Blinking!

---
+++ Assets/Blink.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    private Animator _anim;
+    private float _timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = Random.Range(0f, 7f);
+        _anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+
+        if (_timer <= 0)
+        {
+            Debug.Log(_anim);
+            _timer = Random.Range(1f, 7f);
+            _anim.Play("Blink", 1, 0);
+        }
+    }
+}
